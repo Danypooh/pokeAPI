6305094e9c97d4b48ab51d701b4df7e300d291d6
Danypooh <delatorrechapell@gmail.com>
2022-09-21 12:12:43 -0500

    added image and name of pokemon

--- script.js
+++ script.js
@@ -8,8 +8,12 @@ getData.addEventListener('click', function handleClick() {
 function getMyData(pokemon) {
     $.getJSON("https://pokeapi.co/api/v2/pokemon/" + pokemon,
       function(json) {
-        console.log(json);
-        $('#resultado').text(JSON.stringify(json));
+        $('#resultado').text(JSON.stringify(json.name));
+        let pokeImage = json.sprites.front_default;
+        let image = document.createElement('img');
+        let resultado = document.getElementById('resultado');
+        image.src = pokeImage;
+        resultado.appendChild(image);
       }
     );
     
